1e34dc0f2d72c69f78b07269099b1e19b9ba1a08
ronhadad22 <ronhadad22@gmail.com>
2022-03-19 18:43:38 +0200

    Update monorepo.medium.groovy

--- monorepo.medium.groovy
+++ monorepo.medium.groovy
@@ -4,7 +4,7 @@
 def call() {
     String repositoryName = env.JOB_NAME.split('/')[1]
     String rootFolderPath = "CCV/$repositoryName"
-
+    println "Hello World ${rootFolderPath}"
     List<String> jenkinsfilePaths = provisionItems(rootFolderPath, env.GIT_URL)
     List<String> multibranchPipelinesToRun = findMultibranchPipelinesToRun(jenkinsfilePaths)
     runPipelines(rootFolderPath, multibranchPipelinesToRun)
